5f814d5e1253aafdf6d62bbf6a3376e03f3892e6
himIoT19 <thehimu1990@gmail.com>
2023-08-17 15:47:12 +0530

    Add: Working with Enumerations(enum)

--- main.py
+++ main.py
@@ -1,33 +1,66 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
-mash = FastAPI()
+
+class ModelName(str, Enum):
+    """
+    1. subclass that inherits from str and from Enum
+    2. By inheriting from 'str' the API docs will be able to know that the values must be of type string and will be able to render correctly
+    """
+    # class attributes with fixed values, which will be the available valid values
+    alexnet = 'alexnet'
+    resnet = 'resnet'
+    lenet = 'lenet'
 
 
-@mash.get("/")
+him = FastAPI()
+
+
+@him.get("/")
 async def root():
-    return {"message": "Hello Manansh"}
+    return {"message": "Hello Himanshu"}
 
 
-@mash.get("/items/{item_id}")
+@him.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
 
 
-@mash.get("/users/{user_id}")
+@him.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
 
 
-@mash.get("/users/me")
+@him.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
 
 
-@mash.get("/users")
+@him.get("/users")
 async def read_users():
     return ["Rick", "Morty"]
 
+
 # It will not be counted : The first one will always be used since the path matches first.
-# @mash.get("/users")
+# @him.get("/users")
 # async def read_users2():
 #     return ["Bean", "Elfo"]
+
+
+# create a path parameter with a type annotation using the enum class created (ModelName)
+@him.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    """
+
+    :param model_name:
+    :return:
+    """
+    # Compare enumeration members
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    # Get the enumeration value -> your_enum_member.value (also access the value "lenet" with ModelName.lenet.value)
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
