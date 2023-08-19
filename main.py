10b518fc457bb99f1aeb6aa9a5637fe3cff9d289
himIoT19 <thehimu1990@gmail.com>
2023-08-19 05:40:29 +0530

    Add: working with 'query parameter type conversion'

--- main.py
+++ main.py
@@ -38,9 +38,23 @@ async def read_item(skip: int = 0, limit: int = 10):  # The query is the set of 
 
 
 @him.get("/items/{item_id}")
-async def read_item(item_id: str, q: str | None = None):  # The same way, we can declare optional query parameters, by setting their default to None
-    # eg: http://127.0.0.1:8000/items/1?q=python
-    return {"item_id": item_id, "q": q} if q else {"item_id": item_id}
+async def read_item(item_id: str, q: str | None = None, short: bool = False):  # In this case, if we go to:
+    # eg: http://127.0.0.1:8000/items/foo?short=1 or http://127.0.0.1:8000/items/foo?short=True or http://127.0.0.1:8000/items/foo?short=true
+    # or http://127.0.0.1:8000/items/foo?short=on or http://127.0.0.1:8000/items/foo?short=yes
+    # or any other case variation (uppercase, first letter in uppercase, etc.),
+    # your function will see the parameter short with a bool value of True. Otherwise, as False.
+    item = {"item_id": item_id}
+    if q:
+        item["q"] = q
+    if not short:
+        item["description"] = "This is an amazing item that has a long description"
+    return item
+
+
+# @him.get("/items/{item_id}")
+# async def read_item(item_id: str, q: str | None = None):  # The same way, we can declare optional query parameters, by setting their default to None
+#     # eg: http://127.0.0.1:8000/items/1?q=python
+#     return {"item_id": item_id, "q": q} if q else {"item_id": item_id}
 
 
 # @him.get("/items/{item_id}")
